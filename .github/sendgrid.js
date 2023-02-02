a0f281ccc14f58be04af1f5248e10d61eb18288a
NARENDRA KUMAR <87057358+narendrakumar02@users.noreply.github.com>
2023-02-02 08:33:12 +0530

    Update sendgrid.js

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -4,8 +4,8 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
 
 const msg = {
-    to: 'narendra.kumar02@nagarro.com',
-    from: 'rexzoneandothers@gmail.com',
+    to: 'rexzoneandothers@gmail.com',
+    from: 'narendra.kumar02@nagarro.com',
     subject: 'Hello world',
     text: 'Hello plain world!',
     html: '<p>Hello HTML world!</p>',
